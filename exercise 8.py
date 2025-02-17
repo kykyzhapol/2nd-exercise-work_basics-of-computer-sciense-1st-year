6a4c3b5986163a85b9a4c5ce8a30f3df155f365d
Sergey Chirkov <s.chirkov1@g.nsu.ru>
2025-02-17 14:52:46 +0700

    Update exercise 8.py

--- exercise 8.py
+++ exercise 8.py
@@ -1,2 +1,6 @@
 data_list = input().split()
 print(int(data_list[1])//(int(data_list[0])+1))
+
+#use map function
+#data_list = list(map(int, input().split()))
+#print(data_list[1]//(data_list[0]+1))
